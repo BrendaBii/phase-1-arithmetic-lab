43531d8f0ce5320b03d95d48236dbaee27743546
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-13 15:14:41 +0300

    Completed assignment.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply, num1 = 31, num2 = 2;
+multiply = (num1 * num2);
+multiply;
+
+let random = (Math.floor(Math.random()*100)+1);
+
+let mod, num3 = 18, num4 = 7;
+mod = (num3 % num4);
+mod;
+
+let max;
+max = Math.max(20);
